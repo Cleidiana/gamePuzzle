c36c8e7c7c0fd7ed1434bfe89251608edf87472f
Cleidiana <Cleidianareis@gea.inatel.br>
2021-10-29 17:58:39 -0300

    Add video de intro e menu

---
+++ gameFinal/Assets/Scripts/Menu_inicial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_inicial : MonoBehaviour {
+
+    public string nomeDaCena;
+
+    public void MudaCena() {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+   
+}

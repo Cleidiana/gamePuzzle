78a4a4f60c5a97499cdb82cd8f177e536f23fce7
Cleidiana <Cleidianareis@gea.inatel.br>
2021-11-29 23:18:46 -0300

    add botão para download do manual e assets

--- gameFinal/Assets/Scripts/Menu_inicial.cs
+++ gameFinal/Assets/Scripts/Menu_inicial.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Net;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -15,5 +18,19 @@ public class Menu_inicial : MonoBehaviour {
     {
         Application.Quit();
     }
-   
+    private const string UriString = "https://drive.google.com/uc?export=download&id=1hoY8S4woiHc46_1U8bUAHxZ2D0-wM6oy";
+    private const string fileDownload = "Manual.txt";
+
+    public void Download()
+    {
+        WebClient webClient = new WebClient();
+        webClient.DownloadDataAsync(new Uri(UriString));
+        webClient.DownloadDataCompleted += DownloadTerminou;
+
+
+    }
+    private void DownloadTerminou(object sender, DownloadDataCompletedEventArgs e)
+    {
+        File.WriteAllBytes(fileDownload, e.Result);
+    }
 }
---
+++ gameFinal/Assets/Scripts/Para_video.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Para_video : MonoBehaviour {
+
+    VideoPlayer meuVideo;
+	void Start () {
+        meuVideo = GetComponent<VideoPlayer>();
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            meuVideo.Stop();
+        }
+    }
+}
+
